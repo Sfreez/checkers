aa3bf5de2e3bf83acc4744eb55def6bb8abfeb04
enzo <73506697+Sfreez@users.noreply.github.com>
2023-03-27 21:59:11 +0000

    initialize and print board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    print!("Hello checkers")
+    const BOARD_LENGTH: usize = 10;
+    const PIECES_NUMBER: usize = 20;
+
+    let mut board = [" "; BOARD_LENGTH*BOARD_LENGTH/2];
+    for index in 0..BOARD_LENGTH*BOARD_LENGTH/2 {
+        if index < PIECES_NUMBER {
+            board[index] = "W"
+        } else if index >= BOARD_LENGTH*BOARD_LENGTH/2 - PIECES_NUMBER {
+            board[index] = "B"
+        }
+    }
+
+    for line in 0..BOARD_LENGTH {
+        println!("-----------------------------------------");
+        for column in 0..BOARD_LENGTH {
+            if (line + column) % 2 == 1 {
+                print!("| {} ", board[(line * BOARD_LENGTH + column) / 2]);
+            } else {
+                print!("|   ");
+            }
+        }
+        print!("|");
+        println!();
+    }
+    println!("-----------------------------------------");
 }
